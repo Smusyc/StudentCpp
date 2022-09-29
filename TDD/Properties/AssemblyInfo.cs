9612d7d1db89e26ac34f7cef4e9465955324ef45
Smusyc <yacov.sotin.99@gmail.com>
2022-09-29 15:53:01 +0500

    3rd part

---
+++ TDD/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TDD")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TDD")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("31bbfb4e-57b9-4a9b-ba52-460656018c8b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TDD/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+
+namespace TDD2
+{
+    [TestClass]
+    public class MathUtils
+    {
+        public static int Fibonacci(int n)
+        {
+            if (n == 0)
+                return 0;
+            else
+                return 1;
+        }
+
+        [TestMethod]
+        public void TestFirstFibonacciNumber()
+        {
+            Assert.AreEqual(0, MathUtils.Fibonacci(0));
+        }
+        
+        
+        
+    }
+}
