16c0d66b6094b58fd2860ddd91564b5b2fbba180
Smusyc <yacov.sotin.99@gmail.com>
2022-09-29 16:05:46 +0500

    8th part

---
+++ Синхронизация процесссов и потоков/Синхронизация процесссов и потоков/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Синхронизация_процесссов_и_потоков
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
