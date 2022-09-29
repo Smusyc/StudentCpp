4f18398c44736f2cd3aa179d225b7c3779e3d8cb
Smusyc <yacov.sotin.99@gmail.com>
2022-09-29 15:51:45 +0500

    1st part of package

---
+++ tester/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+namespace tester
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = 0, a=1,b=0;
+            result = ((2 * a + (Math.Abs(a - b) - 1)) * (Math.Abs(a - b) / 2));
+            Console.WriteLine(result);
+        }
+    }
+}
