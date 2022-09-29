f33a4cd1bb1cf3be893c306289d2249417f02262
Smusyc <yacov.sotin.99@gmail.com>
2022-09-29 15:55:44 +0500

    6th part

---
+++ Prog_engi_3/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Prog_engi_3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Prog_engi_3")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d4a19e60-3550-4db2-9b57-1dea500a6d00")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Prog_engi_3/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TDD
+{
+    [TestClass]
+    public class MathUtils
+    {
+        public static int Fibonacci(int n)
+        {
+            return 0;
+        }
+        
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(0, MathUtils.Fibonacci(0));
+        }
+    }
+}
